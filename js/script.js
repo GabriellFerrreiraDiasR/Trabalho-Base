f03d15ebb3bc836489c449434193ad241145ba74
Gabriell <67057880+GabriellFerrreiraDiasR@users.noreply.github.com>
2026-06-17 21:29:51 -0300

    Alterações finais

--- js/script.js
+++ js/script.js
@@ -1,39 +1,52 @@
 function mostrarDados() {
-  let nome = document.getElementById("nome").value;
-  let email = document.getElementById("email").value;
-  let idade = document.getElementById("idade").value;
-  let nascimento = document.getElementById("nascimento").value;
-  let genero = document.getElementById("genero").value;
 
-  let dataFormatada = nascimento
-    ? new Date(nascimento).toLocaleDateString("pt-BR")
-    : "";
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let nascimento = document.getElementById("nascimento").value;
+    let genero = document.getElementById("genero").value;
 
-  document.getElementById("saida").innerHTML = `
+    let dataFormatada = "";
 
-<h3>Dados enviados</h3>
+    if(nascimento){
 
-<p><strong>Nome:</strong> ${nome}</p>
+        let partes = nascimento.split("-");
 
-<p><strong>Email:</strong> ${email}</p>
+        let ano = partes[0];
+        let mes = partes[1];
+        let dia = partes[2];
 
-<p><strong>Idade:</strong> ${idade}</p>
+        dataFormatada = dia + "/" + mes + "/" + ano;
 
-<p><strong>Data de nascimento:</strong> ${dataFormatada}</p>
+    }
 
-<p><strong>Gênero:</strong> ${genero}</p>
+    document.getElementById("saida").innerHTML = `
 
-`;
+    <h3>Dados enviados</h3>
+
+    <p><strong>Nome:</strong> ${nome}</p>
+
+    <p><strong>Email:</strong> ${email}</p>
+
+    <p><strong>Idade:</strong> ${idade}</p>
+
+    <p><strong>Data de nascimento:</strong> ${dataFormatada}</p>
+
+    <p><strong>Gênero:</strong> ${genero}</p>
+
+    `;
+
 }
 
 function limparDados() {
-  document.getElementById("nome").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("idade").value = "";
-  document.getElementById("nascimento").value = "";
-  document.getElementById("genero").value = "";
 
-  document.getElementById("saida").innerHTML = `
-<p>Os dados aparecerão aqui.</p>
-`;
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("nascimento").value = "";
+    document.getElementById("genero").value = "";
+
+    document.getElementById("saida").innerHTML = `
+    <p>Os dados aparecerão aqui.</p>
+    `;
 }
